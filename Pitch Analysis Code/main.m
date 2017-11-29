8497d3c006f260e1469c14eade5273b330eef411
sandidel <sanjeevdidel@gmail.com>
2017-11-29 17:40:03 +0530

    Add files via upload

---
+++ Pitch Analysis Code/main.m
@@ -0,0 +1,6 @@
+%code for pitch analysis of a wav file
+[y,fs] = audioread('a16.wav');%read in the wav file
+[t,f0,avgF0] = pitch(y,fs);%call the pitch.m routine
+plot(t,f0)%plot pitch contour verus time frame
+avgF0%display the average pitch
+%sound(y)%playback the sound file
