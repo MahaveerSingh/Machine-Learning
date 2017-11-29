92ed1d6a0e53ad8ee8f6e84acd9b415c47e6a6f4
sandidel <sanjeevdidel@gmail.com>
2017-11-29 17:41:10 +0530

    Create formant.m

---
+++ Formant Analysis Code/formant.m
@@ -0,0 +1,13 @@
+
+function[P,F,I]=formant(y)
+%calculate the PSD using Yule-Walker's mathod
+order=12;
+P=pyulear(y,order,[]);
+P=10*log10(P);
+F=0:1/128:1;
+
+
+[Pm,I]=pickmax(P);
+%Pm
+%I
+I=I/128;
